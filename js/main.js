fbc2b48b786a254198cdf54b0e4ae7b75952e4b6
Saad Ullah Sajid <saadullahsajid28091998@gmail.com>
2022-05-06 14:52:42 +0500

    Updated Main.js

--- js/main.js
+++ js/main.js
@@ -14,22 +14,29 @@ $(document).ready(function () {
   });
   //   custom cursor code end
 
-  $(window).on('mousemove', function(e) {
+  //   parallax effect on top images on mouse move code start
+  $(window).on("mousemove", function (e) {
     var w = $(window).width();
     var h = $(window).height();
     var offsetX = 0.5 - e.pageX / w;
     var offsetY = 0.5 - e.pageY / h;
 
-    $(".parallax").each(function(i, el) {
-        var offset = parseInt($(el).data('offset'));
-        var translate = "translate3d(" + Math.round(offsetX * offset) + "px," + Math.round(offsetY * offset) + "px, 0px)";
+    $(".parallax").each(function (i, el) {
+      var offset = parseInt($(el).data("offset"));
+      var translate =
+        "translate3d(" +
+        Math.round(offsetX * offset) +
+        "px," +
+        Math.round(offsetY * offset) +
+        "px, 0px)";
 
-        $(el).css({
-            '-webkit-transform': translate,
-            'transform': translate,
-            'moz-transform': translate
-        });
+      $(el).css({
+        "-webkit-transform": translate,
+        transform: translate,
+        "moz-transform": translate,
+      });
     });
-});
+  });
 
+  //   parallax effect on top images on mouse move code end
 });
